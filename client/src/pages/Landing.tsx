f3e19a0c0f0feebf6831a656c056e64378bfb811
marissann11 <wilhite.marissa@gmail.com>
2022-05-30 17:19:13 -0400

    added react spring for landing page

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import LandingNavComponent from "../components/LandingNav";
+import { useSpring, animated } from "react-spring";
+
 import "./styles/landing.scss";
 import "../assets/background.png";
 
 export interface ILandingPageProps {}
 
 const LandingPage: React.FunctionComponent<ILandingPageProps> = (props) => {
+  const mainSpring = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: { duration: 1000 },
+  });
+
   return (
-    <div className="landing">
-      <div className="text">
-        <h1>Marissa Ann Wilhite</h1>
-        <h2>Full Stack Developer</h2>
+    <animated.main style={mainSpring}>
+      <div className="landing">
+        <div className="text">
+          <h1>Marissa Ann Wilhite</h1>
+          <h2>Full Stack Developer</h2>
+        </div>
+        <LandingNavComponent />
       </div>
-      <LandingNavComponent />
-    </div>
+    </animated.main>
   );
 };
 
