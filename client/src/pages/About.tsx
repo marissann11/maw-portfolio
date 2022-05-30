aeab0e4e1d1562a63050353dea0df9bec9f83cd4
marissann11 <wilhite.marissa@gmail.com>
2022-05-30 18:50:38 -0400

    about page styling

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -12,39 +12,39 @@ const AboutPage: React.FunctionComponent<IAboutPageProps> = (props) => {
       <NavComponent />
       <div className="about">
         <div className="aboutText">
-          <h1>About Me</h1>
+          <h1>Hi, I'm Marissa!</h1>
           <p>
             This is a paragraph of text about me.
             <br />
             It will probably say things that I enjoy doing <br />
             Or things about my experience I don't know
           </p>
-        </div>
-        <div className="icons">
-          <a
-            href="https://www.linkedin.com/in/marissa-wilhite-8700a4238/"
-            target="_blank"
-            rel="noreferrer"
-            className="icon"
-          >
-            <FaLinkedin />
-          </a>
-          <a
-            href="https://github.com/marissann11"
-            target="_blank"
-            rel="noreferrer"
-            className="icon"
-          >
-            <FaGithub />
-          </a>
-          <a
-            href="https://stackoverflow.com/users/17353462/marissa"
-            target="_blank"
-            rel="noreferrer"
-            className="icon"
-          >
-            <FaStackOverflow />
-          </a>
+          <div className="icons">
+            <a
+              href="https://www.linkedin.com/in/marissa-wilhite-8700a4238/"
+              target="_blank"
+              rel="noreferrer"
+              className="icon"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              href="https://github.com/marissann11"
+              target="_blank"
+              rel="noreferrer"
+              className="icon"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="https://stackoverflow.com/users/17353462/marissa"
+              target="_blank"
+              rel="noreferrer"
+              className="icon"
+            >
+              <FaStackOverflow />
+            </a>
+          </div>
         </div>
         <div className="img">
           <img src={me} alt="me" />
