fb769186e26144004f6f3368aba9bdbdacb06dcc
marissann11 <wilhite.marissa@gmail.com>
2022-05-30 01:34:22 -0400

    skills page with icons

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavComponent from "../components/Nav";
 import me from "../assets/me.png";
+import "./styles/about.scss";
 import { FaLinkedin, FaGithub, FaStackOverflow } from "react-icons/fa";
 
 export interface IAboutPageProps {}
@@ -9,38 +10,45 @@ const AboutPage: React.FunctionComponent<IAboutPageProps> = (props) => {
   return (
     <div>
       <NavComponent />
-      <h1>About Me</h1>
-      <p>
-        This is a paragraph of text about me.
-        <br />
-        It will probably say things that I enjoy doing <br />
-        Or things about my experience I don't know
-      </p>
-      <div>
-        <img src={me} alt="me" />
-      </div>
-      <div>
-        <a
-          href="https://www.linkedin.com/in/marissa-wilhite-8700a4238/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaLinkedin />
-        </a>
-        <a
-          href="https://github.com/marissann11"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaGithub />
-        </a>
-        <a
-          href="https://stackoverflow.com/users/17353462/marissa"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaStackOverflow />
-        </a>
+      <div className="about">
+        <div className="aboutText">
+          <h1>About Me</h1>
+          <p>
+            This is a paragraph of text about me.
+            <br />
+            It will probably say things that I enjoy doing <br />
+            Or things about my experience I don't know
+          </p>
+        </div>
+        <div className="icons">
+          <a
+            href="https://www.linkedin.com/in/marissa-wilhite-8700a4238/"
+            target="_blank"
+            rel="noreferrer"
+            className="icon"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/marissann11"
+            target="_blank"
+            rel="noreferrer"
+            className="icon"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://stackoverflow.com/users/17353462/marissa"
+            target="_blank"
+            rel="noreferrer"
+            className="icon"
+          >
+            <FaStackOverflow />
+          </a>
+        </div>
+        <div className="img">
+          <img src={me} alt="me" />
+        </div>
       </div>
     </div>
   );
--- client/src/pages/Skills.tsx
+++ client/src/pages/Skills.tsx
@@ -1,22 +1,176 @@
 import React from "react";
 import NavComponent from "../components/Nav";
 import resumeImg from "../assets/resumeImg.png";
+import "./styles/skills.scss";
+import nodeImg from "../assets/backend/nodejs.png";
+import graphqlImg from "../assets/backend/graphql.png";
+import expressImg from "../assets/backend/express.png";
+import prismaImg from "../assets/backend/prisma.png";
+import reactImg from "../assets/frontend/react.png";
+import jsImg from "../assets/frontend/javascript.png";
+import typeImg from "../assets/frontend/typescript.png";
+import htmlImg from "../assets/frontend/html.png";
+import cssImg from "../assets/frontend/css.png";
+import jqueryImg from "../assets/frontend/jquery.png";
+import mysqlImg from "../assets/databases/mysql.png";
+import postImg from "../assets/databases/postgresql.png";
+import mongoImg from "../assets/databases/mongo.png";
 
 export interface ISkillsPageProps {}
 
 const SkillsPage: React.FunctionComponent<ISkillsPageProps> = (props) => {
+  type Skill = {
+    src: string;
+    name: string;
+    backend: boolean;
+    frontend: boolean;
+    database: boolean;
+  };
+
+  let skills: Skill[];
+
+  skills = [
+    {
+      src: nodeImg,
+      name: "Node js",
+      backend: true,
+      frontend: false,
+      database: false,
+    },
+    {
+      src: graphqlImg,
+      name: "GraphQL",
+      backend: true,
+      frontend: false,
+      database: false,
+    },
+    {
+      src: expressImg,
+      name: "Express js",
+      backend: true,
+      frontend: false,
+      database: false,
+    },
+    {
+      src: prismaImg,
+      name: "Prisma ORM",
+      backend: true,
+      frontend: false,
+      database: false,
+    },
+
+    {
+      src: reactImg,
+      name: "React js",
+      backend: false,
+      frontend: true,
+      database: false,
+    },
+    {
+      src: jsImg,
+      name: "Javascript",
+      backend: false,
+      frontend: true,
+      database: false,
+    },
+    {
+      src: typeImg,
+      name: "Typescript",
+      backend: false,
+      frontend: true,
+      database: false,
+    },
+    {
+      src: jqueryImg,
+      name: "JQuery",
+      backend: false,
+      frontend: true,
+      database: false,
+    },
+    {
+      src: htmlImg,
+      name: "HTML 5",
+      backend: false,
+      frontend: true,
+      database: false,
+    },
+    {
+      src: cssImg,
+      name: "CSS",
+      backend: false,
+      frontend: true,
+      database: false,
+    },
+    {
+      src: mysqlImg,
+      name: "MySQL",
+      backend: false,
+      frontend: false,
+      database: true,
+    },
+    {
+      src: postImg,
+      name: "PostgreSQL",
+      backend: false,
+      frontend: false,
+      database: true,
+    },
+    {
+      src: mongoImg,
+      name: "MongoDB",
+      backend: false,
+      frontend: false,
+      database: true,
+    },
+  ];
+
   return (
-    <div>
+    <>
       <NavComponent />
-      <h1>Backend Technologies</h1>
-      <h2>Frontend Technologies</h2>
-      <h2>Deployment & Version Control</h2>
-      <h3>What I'm learning ... </h3>
-      <div>
-        <h3>Download My Resume</h3>
-        <img src={resumeImg} alt="screenshot of resume"></img>
+      <div className="skillsPage">
+        <div className="skills">
+          <h1>Backend Technologies</h1>
+          {skills.map(
+            (skill, i) =>
+              skill.backend && (
+                <div key={i} className="icon">
+                  <img src={skill.src} alt={skill.name}></img>
+                </div>
+              )
+          )}
+          <h2>Databases</h2>
+          {skills.map(
+            (skill, i) =>
+              skill.database && (
+                <div key={i} className="icon">
+                  <img src={skill.src} alt={skill.name}></img>
+                </div>
+              )
+          )}
+          <h2>Frontend Technologies</h2>
+          {skills.map(
+            (skill, i) =>
+              skill.frontend && (
+                <div key={i} className="icon">
+                  <img src={skill.src} alt={skill.name}></img>
+                </div>
+              )
+          )}
+          <h2>Deployment & Version Control</h2>
+          <h4>Heroku, GitHub Pages, MongoDB Atlas, Git, BitBucket, GitHub</h4>
+          <h2>Additional Skills</h2>
+          <h4>
+            ESLint, Prettier, TDD, Jira, Data Visualization, Stripe API, Redux
+          </h4>
+          <h3>What I'm learning ... </h3>
+          <p>C#, .NET, AWS</p>{" "}
+        </div>
+        <div className="resume">
+          <h3>Download My Resume</h3>
+          <img src={resumeImg} alt="screenshot of resume"></img>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
